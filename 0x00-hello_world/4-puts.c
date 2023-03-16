70487a27f04867ba8dd1d74bfa091b5a0b33eb39
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-03-16 11:31:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char ahme[] = "\"Programming is like building a multilingual puzzle";	
-	puts(ahme);
-	return (0);
+char ahme[] = "\"Programming is like building a multilingual puzzle";	
+puts(ahme);
+return (0);
 }
