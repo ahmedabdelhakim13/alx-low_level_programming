0db8f6ccf4c46a76c89be605c20145186e24a6b8
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-03-16 13:58:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return: Always 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
-{	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+{
+	fputs("and that piece of art is useful\" - DoraKorpar, 2015-10-19\n", stdout);
 	return (1);
 }
