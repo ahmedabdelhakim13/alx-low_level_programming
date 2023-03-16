b4d49a753b83f31ef63a73709622264f584198d6
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-03-16 11:55:34 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 int m = 4;
 char h = 'a';
 float n = 12.5;
-printf("Size of a char:%d \nSize of an int:%d \nSize of a long int:%d \n Size of a long long int:%d \nSize of a float:%d \n" sizeof(h) , sizeof(m) ,(long)sizeof(m) , (long long)sizeof(m) , sizeof(n) ); 
+printf("Size of ai char:%d \nSize of an int:%d \nSize of a long int:%d \n Size of a long long int:%d \nSize of a float:%d \n" sizeof(h) , sizeof(m) ,(long) sizeof(m) , (long long) sizeof(m) , sizeof(n) ); 
 return (0);
 }
