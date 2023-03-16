a4feaeff0708f89942133da7d1785e05a03f2a87
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-03-16 11:46:36 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int m=4;
+char h='a';
+float n=12.5;
+printf("integar=%d ,character=%d ,float=%d",sizeof(m),sizeof(h),sizeof(n));
+return (0);
+}
