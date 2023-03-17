42fdb3759ccfe1532c76cf0c8e9495ccf73c5413
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-03-17 01:30:57 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/** entity point=1
+ *
+ * Return: 0
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+	printf("is positive\n");
+
+	else if(n<0)
+		printf("is negative\n");
+
+	else
+		printf("is zero\n");
+	return (0);
+}
