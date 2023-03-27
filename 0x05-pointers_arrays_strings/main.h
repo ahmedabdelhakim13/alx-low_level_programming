fda5e674548294a0508b6edab01b60ee40e85f0a
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-03-27 14:12:44 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,10 +9,10 @@ void reset_to_98(int *n);
 int _putchar(char c);
 
 void swap_int(int *a, int *b);
-
+void _puts(char *str);
 int _strlen(char *s);
 
-void _puts(char *str);
+
 
 void print_rev(char *s);
 
