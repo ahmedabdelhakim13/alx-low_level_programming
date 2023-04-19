ebfbefa31316672d0022090d73f181956ab0a35e
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-04-19 06:30:48 -0700

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef functions_pointers
-#define functions_pointers 
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
 
 #include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
