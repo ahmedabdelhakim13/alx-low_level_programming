571b620e6195cfc8e6e3454f65ff3512675420ef
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-04-19 06:23:14 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,11 @@
  *print_name - prints the name
  *@name: string to be printed
  *@f: pointer to character
- *
- *
- *
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }
